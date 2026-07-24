3e93f96b1efe98e6d54c6216a8786c98de121c9c
gregPerlinLi <lihaolin13@outlook.com>
2026-07-24 20:34:02 +0800

    fix: 移动端标题文字恢复显示, 底栏年份改为自动获取
    - 移动端 navbar-brand span 不再隐藏, 改为缩小字号
    - 底栏版权年份改为 Date.today.year 自动获取
    - 版本号升至 2.0.2

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-Rails.application.config.x.version = "2.0.1"
+Rails.application.config.x.version = "2.0.2"
 
 # Docker Cloud will pass the git commit it is building the image from
 # See: https://docs.docker.com/docker-hub/builds/advanced/
